7791f938a02b6770dabfd17fd5f9f834e1f44264
diane <diane.williams@ucdenver.edu>
2020-08-16 15:31:53 -0600

    adding lesson1

--- intro_python/lesson1/lesson1c.py
+++ intro_python/lesson1/lesson1c.py
@@ -1,22 +1,4 @@
-'''
-lesson 1
-   keyboard input, screen output
-   converting strings to int, float
-'''
-
-''' 
-input function reads from the keyboard
-returns a string type
-assignment operator - stores data returned in a variable
-'''
-
-stringMessage1 = input('Enter a word: ')
-print("stringMessage1: " + stringMessage1)
-print("data type of stringMessage1: " + str(type(stringMessage1)))
-
-stringMessage2 = input("\nenter several words: ")
-print("stringMessage2: " + stringMessage2)
-print("data type of stringMessage2: " + str(type(stringMessage2)))
+# lesson1c - converts strings to integer, float 
 
 # convert the string to an integer
 # if you type a non-numeric value, the conversion to int will fail
